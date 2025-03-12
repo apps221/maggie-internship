65c7a780ef2a6bc147eec57184f6ae705dd4ec90
apps221 <maggie.a.hong@gmail.com>
2025-03-12 12:10:31 -0400

    working on carousel

--- src/components/home/HotCollections.jsx
+++ src/components/home/HotCollections.jsx
@@ -1,15 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import AuthorImage from "../../images/author_thumbnail.jpg";
-import nftImage from "../../images/nftImage.jpg";
 import axios from 'axios';
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const HotCollections = () => {
-  const axios = require('axios');
+   function SimpleSlider() {
+    var settings = {
+      dots: true,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 4,
+      slidesToScroll: 4,
+    };
+  }
+  const [data, setData] = useState([])
     async function fetchHotCollections() {
       try {
           const response = await axios.get('https://us-central1-nft-cloud-functions.cloudfunctions.net/hotCollections');
-          console.log(response.data);
+          setData(response.data);
       } catch (error) {
           console.error('Error fetching data:', error);
       }
@@ -25,29 +35,33 @@ const HotCollections = () => {
               <div className="small-border bg-color-2"></div>
             </div>
           </div>
-          {new Array(4).fill(0).map((_, index) => (
-            <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={index}>
+          <Slider {...HotCollections.settings}>
+          {data.map((card) => (
+            <div>
+              <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={card.id}>
               <div className="nft_coll">
                 <div className="nft_wrap">
-                  <Link to="/item-details">
-                    <img src={nftImage} className="lazy img-fluid" alt="" />
+                  <Link to={`/item-details/${card.id}`}>
+                    <img src={card.nftImage} className="lazy img-fluid" alt="" />
                   </Link>
                 </div>
                 <div className="nft_coll_pp">
                   <Link to="/author">
-                    <img className="lazy pp-coll" src={AuthorImage} alt="" />
+                    <img className="lazy pp-coll" src={card.AuthorImage} alt="" />
                   </Link>
                   <i className="fa fa-check"></i>
                 </div>
                 <div className="nft_coll_info">
                   <Link to="/explore">
-                    <h4>Pinky Ocean</h4>
+                    <h4>{card.title}</h4>
                   </Link>
-                  <span>ERC-192</span>
+                  <span>ERC-{card.code}</span>
                 </div>
               </div>
             </div>
+            </div>
           ))}
+            </Slider>
         </div>
       </div>
     </section>
